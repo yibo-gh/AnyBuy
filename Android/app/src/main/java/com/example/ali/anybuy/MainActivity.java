86f6b8c1d3546250a246c8eaee1830a7f77da9d8
Yang Chen <ychen165@ucsc.edu>
2018-04-30 14:02:58 -0700

    Login

--- Android/app/src/main/java/com/example/ali/anybuy/MainActivity.java
+++ Android/app/src/main/java/com/example/ali/anybuy/MainActivity.java
@@ -5,8 +5,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public EditText email1;
+    public EditText password1;
+
+    public Button login1;
+    String emailstr1;
+    String passwordstr1;
+
+    String combine1;
 
 
     @Override
@@ -14,14 +25,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        email1 = (EditText) findViewById(R.id.emailEditTextID1);
+        password1 = (EditText) findViewById(R.id.passwordEditTextID1);
+
         Button logIn = (Button) findViewById(R.id.loginID);
 
-        Button register = (Button) findViewById(R.id.registerButtonID);
+        Button register1 = (Button) findViewById(R.id.registerButtonID1);
 
         //open the home page if login button was clicked
         logIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                emailstr1 = email1.getText().toString();
+                passwordstr1 = password1.getText().toString();
+
+               // if(!repeatPassword.getText().toString().equals(password1str))
+               // {
+                //    Toast.makeText(MainActivity.this, "Email or Username is incorrect!", Toast.LENGTH_LONG).show();
+                //}
+
+
+
+                //else{
+                    Toast.makeText(MainActivity.this, "You are good", Toast.LENGTH_LONG).show();
+                    combine1 = "reg&" + emailstr1 + "?" + passwordstr1 + "&useSSL=true";
+                 //   String res = SocketClient.run(combine1);
+                  //  System.out.println(res);
+                    System.out.println(combine1);
+                //}
+
 
                 //if the email and password were walid, then go to home page
                 Intent intent = new Intent(MainActivity.this, HomeActivity.class);
@@ -30,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         //open the register page if register button was clicked
-        register.setOnClickListener(new View.OnClickListener() {
+        register1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
--- Android/app/src/main/java/com/example/ali/anybuy/RegisterActivity.java
+++ Android/app/src/main/java/com/example/ali/anybuy/RegisterActivity.java
@@ -1,12 +1,10 @@
 package com.example.ali.anybuy;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
@@ -32,7 +30,7 @@ public class RegisterActivity extends AppCompatActivity {
         repeatPassword = (EditText) findViewById(R.id.repeatPassEditTextID);
 
 
-        register = (Button) findViewById(R.id.registerButtonID);
+        register = (Button) findViewById(R.id.registerButtonID1);
 
 
 
