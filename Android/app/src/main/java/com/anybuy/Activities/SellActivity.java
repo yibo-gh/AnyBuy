3d3268c6021f4adf9be6752abc8c1a259d974d2d
Tongxuan <yguo25@ucsc.edu>
2018-05-14 04:41:38 -0700

    bugs fixed.

--- Android/app/src/main/java/com/anybuy/Activities/SellActivity.java
+++ Android/app/src/main/java/com/anybuy/Activities/SellActivity.java
@@ -4,7 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.anybuy.R;
-import com.example.ali.anybuy.R;
 
 public class SellActivity extends AppCompatActivity {
 
