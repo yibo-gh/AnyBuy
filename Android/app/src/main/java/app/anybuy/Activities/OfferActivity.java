520f9a7c6c7e98390bfa8898029b19392fe87810
Tongxuan <yguo25@ucsc.edu>
2018-05-25 08:25:47 -0700

    minor update

---
+++ Android/app/src/main/java/app/anybuy/Activities/OfferActivity.java
@@ -0,0 +1,15 @@
+package app.anybuy.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import app.anybuy.R;
+
+public class OfferActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_offer);
+    }
+}
