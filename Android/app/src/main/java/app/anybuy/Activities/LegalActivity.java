79cb61bae1d6800c65dd9b442d099a0d8c70dc90
Tongxuan <yguo25@ucsc.edu>
2018-05-28 15:46:55 -0700

    bugs fixed.

--- Android/app/src/main/java/app/anybuy/Activities/LegalActivity.java
+++ Android/app/src/main/java/app/anybuy/Activities/LegalActivity.java
@@ -2,6 +2,7 @@ package app.anybuy.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.ViewGroup;
 import android.widget.TableLayout;
 import android.widget.TableRow;
@@ -13,6 +14,13 @@ public class LegalActivity extends AppCompatActivity {
 
     private final int WC = ViewGroup.LayoutParams.WRAP_CONTENT;
     private final int FP = ViewGroup.LayoutParams.FILL_PARENT;
+
+    @Override
+    public boolean onKeyDown(int keyCode,KeyEvent event){
+        if(keyCode==KeyEvent.KEYCODE_BACK)
+            return true;//不执行父类点击事件
+        return super.onKeyDown(keyCode, event);//继续执行父类其他点击事件
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
