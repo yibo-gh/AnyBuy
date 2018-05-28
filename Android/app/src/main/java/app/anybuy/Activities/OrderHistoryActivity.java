3e9975a91f0428b84563358d045950fb08edf232
Hon Yu Ho <honyuho@eduroam-169-233-197-37.ucsc.edu>
2018-05-28 16:11:58 -0700

    change to cancelOrder button

--- Android/app/src/main/java/app/anybuy/Activities/OrderHistoryActivity.java
+++ Android/app/src/main/java/app/anybuy/Activities/OrderHistoryActivity.java
@@ -1,5 +1,6 @@
 package app.anybuy.Activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
@@ -84,6 +85,8 @@ public class OrderHistoryActivity extends AppCompatActivity {
                                         catch (Exception e) {
                                             e.printStackTrace();
                                         }
+                                        Intent intent = new Intent(OrderHistoryActivity.this, OrderHistoryActivity.class);
+                                        startActivity(intent);
                                     }
                                 });
                                 tableRow.addView(bt);
