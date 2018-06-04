395eb85a6c68010f9fb0998e6a8d47fd59dc7b84
Ali Babaei <alibabaei12@gmail.com>
2018-06-03 22:31:26 -0700

    offer page almost there

--- Android/app/src/main/java/app/anybuy/Activities/OfferActivity.java
+++ Android/app/src/main/java/app/anybuy/Activities/OfferActivity.java
@@ -1,26 +1,63 @@
 package app.anybuy.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import  Object.*;
 
+import app.anybuy.Clients.SocketClient;
 import app.anybuy.R;
 public class OfferActivity extends AppCompatActivity {
+    private static String offer;
+    private static String express;
+    private static String remarkstr;
 
+    public static String getRemarkstr() {
+        return remarkstr;
+    }
+
+    public static void setRemark(String remark) {
+        OfferActivity.remarkstr = remark;
+    }
+
+    public static void setOffer(String offer) {
+        OfferActivity.offer = offer;
+    }
+
+    public static void setExpress(String express) {
+        OfferActivity.express = express;
+    }
+
+    public static String getOffer() {
+        return offer;
+    }
+
+    public static String getExpress() {
+        return express;
+    }
+
+    EditText expressCost;
+    EditText makeOffer;
+    EditText remark;
+
+    boolean offerBool;
+    String []sID;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_offer);
 
-        boolean offerBool = false;
-        EditText makeOffer = (EditText) findViewById(R.id.makeOfferEditTextID);
+         offerBool = false;
+         makeOffer = (EditText) findViewById(R.id.makeOfferEditTextID);
+
 
         TextView display =  (TextView) findViewById(R.id.offerPageTextView);
-        EditText expressCost = (EditText) findViewById(R.id.expressCostEditViewID);
-        EditText remark = (EditText)findViewById(R.id.remarkEditTextID);
+         expressCost = (EditText) findViewById(R.id.expressCostEditViewID);
+         remark = (EditText)findViewById(R.id.remarkEditTextID);
 
         Button sendOfferButton = (Button) findViewById(R.id.sendOfferButtonID) ;
         String dataOrders = getIntent().getStringExtra("myData");
@@ -28,7 +65,7 @@ public class OfferActivity extends AppCompatActivity {
         //split the string to get the different info
         String[] splitByNextLine = dataOrders.split("\n");
 
-        String getRemark = remark.getText().toString();
+        final String getRemark = remark.getText().toString();
         for(int i = 0 ; i < splitByNextLine.length; i++)
         {
             System.out.println(splitByNextLine[i] + " ................. ");
@@ -52,21 +89,43 @@ public class OfferActivity extends AppCompatActivity {
         display.setText(dataOrders);
         System.out.println("the imageID is: " + imageID);
 
-
-
-        //get the offer seller wants to make
-        final String offerMade = makeOffer.getText().toString();
-        int offerMadeInt = Integer.parseInt(offerMade);
-
-
-        int expressC = Integer.parseInt(expressCost.getText().toString());
-        System.out.println(imageID + "\n id: " + MainActivity.getID() + "\n rate: " +  offerMade + "\n express cost: " + expressC + "\nremark: " + getRemark );
+        sID = MainActivity.getID().split("\\?");
+        //System.out.println(imageID + "\n id: " + MainActivity.getID() + "\n rate: " +  offerMade + "\n express cost: " + expressC + "\nremark: " + getRemark );
         //when offerbutton is clicked send Data
         sendOfferButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                //Offer makeOffer = new Offer(imageID, MainActivity.getID(), offerMade, ,offerBool, getRemark );
+                setOffer(makeOffer.getText().toString());
+                setExpress(expressCost.getText().toString());
+                setRemark(remark.getText().toString());
+                //get the offer seller wants to make
+
+               int offerMadeInt = Integer.parseInt(getOffer());
+
+               int expressC = Integer.parseInt(getExpress());
+
+                System.out.println(imageID + "\n id: " + MainActivity.getID() + "\n rate: " + offerMadeInt + "\n express cost: " + expressC + "\nremark: " + getRemarkstr() );
+                Offer makeOffer = new Offer(imageID,sID[0], offerMadeInt, expressC, 1 ,offerBool, getRemarkstr() );
+
+                LinkedList linkedList= new LinkedList();
+                linkedList.insert("gvr");
+                linkedList.insert(MainActivity.getID());
+                linkedList.insert(makeOffer);
+
+                try{
+                    String o = (String) SocketClient.Run(linkedList);
+                }
+                catch (Exception e)
+                {
+                    System.out.println("didnt work");
+                }
+
+
+
+                Intent intent = new Intent(OfferActivity.this, SellActivity.class);
+                startActivity(intent);
+
             }
         });
 
